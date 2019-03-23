8cb50fc64a2a193c7a9bb4f3e9c08430e5099d82
jmnelson12 <jacob_m_nelson@live.com>
2019-03-23 16:52:23 -0600

    set up call to auto tempest and base cron

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
 const express = require('express');
+const helmet = require('helmet')
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 8080;
+const { getAutoTempestResults } = require('./lib/tempest');
+const { auto } = require('./lib/cron');
+
+app.use(cors());
+app.use(helmet());
+app.disable('x-powered-by');
+
+app.get('/getVehicles', async (req, res, next) => {
+    console.log("--------------------");
+    console.log("GETTING VEHICLES");
+    const { params } = req.params;
+
+    const autoTempestResults = await getAutoTempestResults(params);
+
+    console.log("DONE GETTING VEHICLES");
+    console.log("--------------------\n");
+
+    res.json({
+        vehicles: autoTempestResults
+    })
+});
+
+app.get(`/startCron`, (req, res, next) => {
+    const init = auto("*/10 * * * * *");
+    init.leCron().start();
+
+    return res.json("success");
+});
+
+// async function testing() {
+//     const autoTempestResults = await getAutoTempestResults();
+//     console.log(autoTempestResults);
+// }
+// testing();
+
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
