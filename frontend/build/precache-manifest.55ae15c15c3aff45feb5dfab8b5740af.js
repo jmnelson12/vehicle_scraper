adc2a59f3a7edec67ae0e47ecc7679fcd6764577
jmnelson12 <jacob_m_nelson@live.com>
2019-03-28 22:15:45 -0600

    fixing prod database

---
+++ frontend/build/precache-manifest.55ae15c15c3aff45feb5dfab8b5740af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fb1c4e6d7eacd746d04",
+    "url": "/static/js/runtime~main.5fb1c4e6.js"
+  },
+  {
+    "revision": "7160fcc3c1259c3b7a2b",
+    "url": "/static/js/main.7160fcc3.chunk.js"
+  },
+  {
+    "revision": "6a3b017b72aa749d1e66",
+    "url": "/static/js/6.6a3b017b.chunk.js"
+  },
+  {
+    "revision": "4afaaf39482d361a0762",
+    "url": "/static/js/5.4afaaf39.chunk.js"
+  },
+  {
+    "revision": "f6b3cf92daa6526dcff2",
+    "url": "/static/js/4.f6b3cf92.chunk.js"
+  },
+  {
+    "revision": "c09f962af987f5df2196",
+    "url": "/static/js/3.c09f962a.chunk.js"
+  },
+  {
+    "revision": "fb3fe463e59c0b43a227",
+    "url": "/static/js/2.fb3fe463.chunk.js"
+  },
+  {
+    "revision": "302c85b6fbdfdc634f82",
+    "url": "/static/js/0.302c85b6.chunk.js"
+  },
+  {
+    "revision": "fb3fe463e59c0b43a227",
+    "url": "/static/css/2.e071be89.chunk.css"
+  },
+  {
+    "revision": "da7a5f149435b1825aec8ae509c0b3c7",
+    "url": "/index.html"
+  }
+];
